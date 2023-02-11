088f84bf9c61c776a44340c4df45cff8499564e7
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-02-12 02:31:22 +0600

    statusbar updated

--- App.js
+++ App.js
@@ -3,5 +3,10 @@ import { StyleSheet, Text, View } from 'react-native'
 import CategoriesScreen from './screens/ CategoriesScreen'
 
 export default function App() {
-  return <CategoriesScreen />
+  return (
+    <>
+      <StatusBar style='light' />
+      <CategoriesScreen />
+    </>
+  )
 }
